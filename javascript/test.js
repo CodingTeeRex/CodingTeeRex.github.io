49115884117ae7faa8bf0a8766a10b1f5750a855
CodingTeeRex <ivanpjh@gmail.com>
2021-06-28 19:51:07 +0800

    updates 28/06/21

---
+++ javascript/test.js
@@ -0,0 +1,5 @@
+let arr = ['abc', '1234'];
+let txt = 'a'
+
+txt = arr[0].substring(0, txt.length + 1);
+console.log(txt);
---
+++ javascript/typewriter.js
@@ -0,0 +1,89 @@
+class TypeWriter {
+    constructor(textElement, mainText, wait = 1500) {
+        this.textElement = textElement;
+        this.mainText = mainText;
+        this.wait = wait;
+        this.txt = '';
+        this.isMainTextComplete = false;
+        this.isDeleting = false;
+        this.wordBank = ['Ivan!', 'CodingTeeRex'];
+        this.currentWordIdx = 0;
+        this.type();
+    }
+
+    type() {
+        let typeSpeed = 50;
+        if (!this.isMainTextComplete) {
+            // Type main text
+
+            console.log("Typing Main Text");
+            this.txt = this.mainText.substring(0, this.txt.length + 1);
+            console.log(this.txt.length);
+            console.log("this.txt: " + this.txt);
+            
+            // Insert word into HTML
+            this.textElement.innerHTML = `<span id="main-title">${this.txt}</span>`;
+
+            if (this.txt === this.mainText) {
+                typeSpeed = 800;
+                this.isMainTextComplete = true;
+                this.txt = ''
+                console.log(this.isMainTextComplete);
+            }
+
+        } else {
+            typeSpeed = 100;
+            console.log("Main Text Complete");
+            console.log(this.txt);
+
+            // Type secondary text
+            const index = this.currentWordIdx % this.wordBank.length; // get idx of current word in wordBank
+            const currentWord = this.wordBank[index]; // get current word
+            console.log("Current Idx: " + index + "\n" + "Current Word: " + currentWord);
+
+            if (this.isDeleting) {
+                console.log("del");
+                typeSpeed /= 2;
+                this.txt = currentWord.substring(0, this.txt.length - 1);
+                console.log("before");
+                console.log("this.txt: " + this.txt);
+                console.log("after");
+            } else {
+                console.log("!del");
+                console.log("this.txt bf: " + this.txt);
+                this.txt = currentWord.substring(0, this.txt.length + 1);
+                console.log("this.txt af: " + this.txt);
+                console.log(this.txt.length);
+                console.log(currentWord[1]);
+            }
+            
+            // Insert secondary text into HTML
+            // this.textElement = document.querySelector('.welcome-title');
+            console.log("Type Sec");
+            this.textElement = document.getElementById('main-title');
+            this.textElement.innerHTML = `<span>${this.mainText} ${this.txt}</span>`;
+
+            if (!this.isDeleting && this.txt === currentWord) {
+                console.log("del");
+                typeSpeed = this.wait;
+                this.isDeleting = true;
+            } else if (this.isDeleting && this.txt === '') {
+                this.isDeleting = false;
+                console.log("!del");
+                this.currentWordIdx++;
+                typeSpeed = 3000;
+            }
+            // typeSpeed = 5000;
+        }
+        setTimeout(() => this.type(), typeSpeed);
+    }
+}
+
+const init = () => {
+    const txtElement = document.querySelector('.welcome-title');
+    const displayTextMain = "Hello, my name is ";
+    new TypeWriter(txtElement, displayTextMain);
+    console.log("init");
+}
+
+document.addEventListener('DOMContentLoaded', init);
