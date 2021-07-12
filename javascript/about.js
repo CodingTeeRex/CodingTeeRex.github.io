226c95b0780b494aa1de7b4cd09e3497a85a66e2
CodingTeeRex <ivanpjh@gmail.com>
2021-07-12 09:19:23 +0800

    about animations update

--- javascript/about.js
+++ javascript/about.js
@@ -45,14 +45,21 @@ class bouncingLetters {
 const sections = document.querySelectorAll("section");
 const options = {
     root: null,
-    rootMargin: "-150px",
-    threshold: 0
+    rootMargin: "25px",
+    threshold: 0.1
 };
 
 const aboutMe = document.querySelector("#about");
 const aboutObs = new IntersectionObserver(function(entries, observer) {
-    gsap.fromTo(".about-body-item", {opacity: 0, x: "-10%"}, {duration: 1.5, x: 0, opacity: 1, display: "block"});
-    gsap.fromTo(".skills", {opacity: 0, x: "10%"}, {duration: 1.5, x: 0, opacity: 1, display: "block"})
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            gsap.fromTo(".about-body-item", {opacity: 0, x: "-10%"}, {duration: 1.5, x: 0, opacity: 1, display: "block"});
+            gsap.fromTo(".skills", {opacity: 0, x: "10%"}, {duration: 1.5, x: 0, opacity: 1, display: "block"})
+        } else {
+            gsap.fromTo(".about-body-item", {x: 0, opacity: 1}, {duration: 0.1, x: "-10%", opacity: 0, display: "none"});
+            gsap.fromTo(".skills", {opacity: 1, x: 0}, {duration: 0.1, x: "-10%", opacity: 0, display: "none"})
+        }
+    })
 }, options);
 
 aboutObs.observe(aboutMe);
